e41ca7b002d21912cd7d146fe96973999ae1c174
JOple <jose_jaena_ople@dsu.edu.ph>
2017-10-19 14:52:09 +0800

    Fixed bugs

--- the-room/src/com/droom/scenes/SceneQ3W.java
+++ the-room/src/com/droom/scenes/SceneQ3W.java
@@ -7,8 +7,9 @@ import com.droom.world.WorldScene;
 public class SceneQ3W extends WorldScene {
 
 	private int cycle = 0;
-	private String[] inspectReplies = new String[] {"There are 3 heads", "There is 1 human head", "There are 2 dragon heads", "Remember 1 and 2"};
-	
+	private String[] inspectReplies = new String[] { "There are 3 heads", "There is 1 human head",
+			"There are 2 dragon heads", "Remember 1 and 2" };
+
 	public SceneQ3W() {
 		super(R.Img.QUADRANT_3_WEST);
 	}
@@ -18,11 +19,13 @@ public class SceneQ3W extends WorldScene {
 		super.onCmdLookLeft(cmd);
 		goTo(SceneQ3S.class);
 	}
+
 	@Override
 	public void onCmdLookBack(SceneCommand cmd) {
 		super.onCmdLookBack(cmd);
 		goTo(SceneQ3E.class);
 	}
+
 	@Override
 	public void onCmdLookRight(SceneCommand cmd) {
 		super.onCmdLookRight(cmd);
@@ -34,6 +37,7 @@ public class SceneQ3W extends WorldScene {
 		say("Back walking");
 		goTo(SceneQ4W.class);
 	}
+
 	@Override
 	public void onCmdMoveRight(SceneCommand cmd) {
 		say("Sidestep right");
@@ -44,8 +48,9 @@ public class SceneQ3W extends WorldScene {
 	public void onCmdInspect(SceneCommand cmd) {
 		say("There are Paintings");
 	}
+
 	@Override
-	public void onCmdInspectClock(SceneCommand cmd) {
+	public void onCmdInspectPainting(SceneCommand cmd) {
 		say(inspectReplies[cycle++ % inspectReplies.length]);
 	}
 }
