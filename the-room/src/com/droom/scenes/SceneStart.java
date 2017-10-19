7e15b4f9789e6b599902a21b3a71b7f1c791ba3a
JOple <jose_jaena_ople@dsu.edu.ph>
2017-10-19 12:37:52 +0800

    Update SceneStart

--- the-room/src/com/droom/scenes/SceneStart.java
+++ the-room/src/com/droom/scenes/SceneStart.java
@@ -1,5 +1,6 @@
 package com.droom.scenes;
 
+import com.droom.SceneCommand;
 import com.droom.utils.R;
 import com.droom.world.WorldScene;
 
@@ -9,10 +10,31 @@ public class SceneStart extends WorldScene {
 		super(R.Img.START);
 	}
 
+	private int cycle;
+
 	@Override
 	public void initialization() {
 		super.initialization();
-		say("You opened you eyes in a unknown room", "You need to get out", "You get off the bed");
-		goTo(SceneQ1S.class);
+		cycle = 0;
+	}
+
+	private void next() {
+		cycle++;
+		switch (cycle) {
+		case 1:
+			say("You opened you eyes in a unknown room");
+			break;
+		case 2:
+			say("You need to get out");
+			break;
+		default:
+			say("You get off the bed");
+			goTo(SceneQ1S.class);
+		}
+	}
+
+	@Override
+	public void perform(SceneCommand cmd) {
+		next();
 	}
 }
