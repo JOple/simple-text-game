54def1afc2ee5b6c8dff0705f1c7ebf454e189e6
JOple <jose_jaena_ople@dsu.edu.ph>
2017-10-19 12:31:11 +0800

    Update UI

--- the-room/src/com/droom/ui/GameUI.java
+++ the-room/src/com/droom/ui/GameUI.java
@@ -3,8 +3,9 @@ package com.droom.ui;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.GridLayout;
+import java.awt.Font;
 import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.KeyAdapter;
@@ -17,6 +18,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
@@ -26,7 +28,8 @@ public class GameUI implements World.CommandActuator {
 
 	private JFrame frame;
 	private JLabel image;
-	private JPanel logs;
+	//	private JPanel logs;
+	private JTextArea area;
 	private JTextField input;
 
 	private Image shown;
@@ -35,9 +38,13 @@ public class GameUI implements World.CommandActuator {
 		image = new JLabel();
 		image.setBackground(Color.BLACK);
 
-		logs = new JPanel(new GridLayout(0, 1));
+		//		logs = new JPanel(new GridLayout(0, 1));
+		area = new JTextArea();
+		area.setEditable(false);
+		area.setFont(new Font("Consolas", Font.PLAIN, 14));
 
-		JScrollPane scrollLog = new JScrollPane(logs);
+		JScrollPane scrollLog = new JScrollPane(area);
+		//		JScrollPane scrollLog = new JScrollPane(logs);
 		scrollLog.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		scrollLog.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 
@@ -64,10 +71,10 @@ public class GameUI implements World.CommandActuator {
 		input.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent e) {
-				if(e.getKeyChar() == '\n') {
+				if (e.getKeyChar() == '\n') {
 					String txt = input.getText();
-					if(txt != null) {
-						if(txt.length() > 1)
+					if (txt != null) {
+						if (txt.length() > 1)
 							logRight(txt);
 						GameUI.this.input(txt);
 						input.setText(null);
@@ -82,53 +89,62 @@ public class GameUI implements World.CommandActuator {
 			}
 		});
 
-		frame.setSize(300, 300);
 		frame.setVisible(true);
 
-		say("> Game Begin");
+		say("Game Begin");
+
+		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+
+		frame.setSize((int) (d.getWidth() * 0.9), (int) (d.getHeight() * 0.9));
 	}
 
 	private void logLeft(String msg) {
-		JLabel box = new JLabel("> " + msg);
-		box.setMinimumSize(new Dimension(100, 40));
-		box.setMaximumSize(new Dimension(100, 40));
-
-		logs.add(box);
-		logs.repaint();
-		logs.revalidate();
+		//		JLabel box = new JLabel("> " + msg);
+		//		box.setMinimumSize(new Dimension(100, 40));
+		//		box.setMaximumSize(new Dimension(100, 40));
+		//
+		//		logs.add(box);
+		//		logs.repaint();
+		//		logs.revalidate();
+		area.setText(area.getText() + "\n>>> " + msg);
 	}
-	private void logRight(String msg) {
-		JLabel box = new JLabel(msg + " <");
-		box.setHorizontalAlignment(SwingConstants.RIGHT);
-		box.setMinimumSize(new Dimension(100, 40));
-		box.setMaximumSize(new Dimension(100, 40));
 
-		logs.add(box);
-		logs.repaint();
-		logs.revalidate();
+	private void logRight(String msg) {
+		//		JLabel box = new JLabel(msg + " <");
+		//		box.setHorizontalAlignment(SwingConstants.RIGHT);
+		//		box.setMinimumSize(new Dimension(100, 40));
+		//		box.setMaximumSize(new Dimension(100, 40));
+		//
+		//		logs.add(box);
+		//		logs.repaint();
+		//		logs.revalidate();
+		area.setText(area.getText() + "\nMe > " + msg);
 	}
 
 	@Override
 	public void say(String... msgs) {
-		for(String msg : msgs)
+		for (String msg : msgs)
 			logLeft(msg);
 	}
+
 	@Override
 	public void ask(String msg, Consumer<String> handler) {
 		throw new UnsupportedOperationException("No implementation of ask method");
 	}
+
 	@Override
 	public void show(Image img) {
 		shown = img;
-		if(img == null) {
+		if (img == null) {
 			image.setIcon(null);
 			return;
 		}
 		ImageIcon icon = new ImageIcon(img);
-		double boxW = image.getWidth(), boxH = image.getHeight(), imgW = icon.getIconWidth(), imgH = icon.getIconHeight();
+		double boxW = image.getWidth(), boxH = image.getHeight(), imgW = icon.getIconWidth(),
+				imgH = icon.getIconHeight();
 		double boxRatio = boxW / boxH, imgRatio = imgW / imgH;
 		double nW = imgW, nH = imgH;
-		if(boxRatio < imgRatio) {
+		if (boxRatio < imgRatio) {
 			nW = boxW;
 			nH = boxW / imgRatio;
 		} else {
